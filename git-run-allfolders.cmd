4032e5203d5e9b350f830087f2f8ca95e07afc5e
George K. <gkorobkov@gmail.com>
2025-11-30 10:51:23 +0300

    feat(git): add conditional execution for git update steps
    Add conditional logic to git-update.cmd to control auto-stash, fetch, and checkout operations based on environment variables. Improve output formatting in git-run-allfolders.cmd with better echo statements and support for update command.

--- git-run-allfolders.cmd
+++ git-run-allfolders.cmd
@@ -7,17 +7,22 @@ REM If no folder_name is provided, uses ".git"
 setlocal EnableDelayedExpansion
 
 set "run_command=%~1"
+if "%run_command%"=="update" set "run_command=git-update.cmd ""%%path_to_git_folder%%"" "
 if "%run_command%"=="" set "run_command=git -C ""%%path_to_git_folder%%"" status -s -b -v"
 
-echo run_command: !run_command!
+echo.
+echo *************************************************************
+
+echo * run_command: !run_command!
 
 set "search_path=%~2"
 if "%search_path%"=="" set "search_path=%cd%"
-echo search_path: !search_path!
+echo * search_path: !search_path!
 
 set "search_folder=%~3"
 if "%search_folder%"=="" set "search_folder=.git"
-echo search_folder: !search_folder!
+echo * search_folder: !search_folder!
+echo *************************************************************
 
 
 echo.
@@ -39,11 +44,12 @@ if exist "!current_path!\!search_folder!" (
     echo *************************************************************
     set "path_to_git_folder=!current_path!"
     echo Running:
-    call echo %run_command%
+    echo %run_command%
     call %run_command%
 ) else (
     rem echo Searching "%search_folder%" folder in: !current_path!
     for /d %%d in ("!current_path!\*") do (
+        rem echo Searching "%search_folder%" folder in: !current_path!
         call :search_folders "%%d"
     )
 )
